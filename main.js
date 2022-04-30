8f4fbd552085fae1718bc7d18471ccaf127db2f8
Justin Katasi <justinkatasi.jk@gmail.com>
2022-04-30 13:49:09 +0200

    Chargement des nouveaux fichiers

--- main.js
+++ main.js
@@ -1,14 +1,83 @@
-//importation de fichier json
-import jsonData from './pando_data.json'; 
 
-console.log(jsonData);
+//Ajax pour récupérer les données json
+const getData = async () => {
+    try{
+        const response = await fetch("pando_data.json");
+        return await response.json();
+    } catch(error) {
+        console.log(error);
+    }
+}
 
-//les données 
+//Récupérer les données
+getData().then(datapoints   => {
+    //Filtre et récupérer le type unique et leurs valeurs 
+    const typeUnique = [];
+
+    datapoints.map(item => {
+        if (!typeUnique.includes(item.measure_type)){
+            typeUnique.push(item.measure_type);
+        }
+        return (typeUnique)
+    });
+
+    //CO2
+    let c02Total = 0;
+    
+    datapoints.map(item => {
+        if (item.measure_type == 'CO2'){
+            c02Total += item.measure_float;
+        }
+        return (c02Total)
+    })
+
+    //HUM
+    let humTotal = 0;
+
+    datapoints.map(item => {
+        if (item.measure_type == 'HUM'){
+            humTotal += item.measure_float;
+        }
+        return (humTotal)
+    })
+
+    //TMP
+    let tmpTotal = 0 ;
+
+    datapoints.map(item => {
+        if (item.measure_type == 'TMP'){
+            tmpTotal += item.measure_float;
+        }
+        return (tmpTotal)
+    })
+
+    //VOCT
+    let voctTotal = 0 ;
+
+    datapoints.map(item => {
+        if (item.measure_type == 'VOCT'){
+            voctTotal += item.measure_float;
+        }
+        return (voctTotal)
+    })
+
+    //tableau de valeur de chaque type de pollution de l'air
+    const tabValeurs = [c02Total, humTotal, tmpTotal, voctTotal];
+   
+    //Mise à jour des informations à affciher 
+    mychart.config.data.labels = typeUnique;
+    mychart.config.data.datasets[0].data = tabValeurs;
+    mychart.update();
+
+})
+
+
+//Les données Chart.js 
 const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: [],
     datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Visualisation des données des capteurs de l’environnement ',
+        data: [],
         backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
@@ -28,7 +97,8 @@ const data = {
         borderWidth: 1
     }]
 };
-//Configuration du graphique
+
+//Configuration du chart.js
 const config = {
     type: 'bar',
     data,
@@ -43,7 +113,4 @@ const config = {
 //créer une instance graphique
 const mychart = new Chart(document.getElementById('myChart'), config);
 
-//Ajax
-const getData = async () => {
-    const data = fetch('./pando_data.json');
-}
+
